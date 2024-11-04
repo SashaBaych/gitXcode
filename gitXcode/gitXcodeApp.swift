5c8a246705c1e8006a2fe3055b2c21583491a799
Sasha Baych <zima.zimushka@gmail.com>
2024-11-04 10:05:06 +0100

    Initial Commit

---
+++ gitXcode/gitXcodeApp.swift
@@ -0,0 +1,17 @@
+//
+//  gitXcodeApp.swift
+//  gitXcode
+//
+//  Created by Sasha Baych on 11/4/24.
+//
+
+import SwiftUI
+
+@main
+struct gitXcodeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
